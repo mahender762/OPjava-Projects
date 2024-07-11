c04b3c55c8bc87a2d0a2e97f162f55c10f7cdded
mahender762 <mahimahender762@gmail.com>
2024-07-11 16:26:52 +0530

    my first module

---
+++ class Odd.java
@@ -0,0 +1,14 @@
+class Odd
+{
+  Public static void main(String args[])
+{
+  int a=20;
+  
+  if(a%2==1)
+   System.out.println("Given number is odd");
+
+  else
+   System.out.println("Given number is even");
+
+}
+}
---
+++ class Subtraction.java
@@ -0,0 +1,10 @@
+class Subtraction
+{
+  Public static void main(String args[])
+{
+  int a=20;
+   int b=20;
+  int sum=a-b;
+Sysyem.out.println(sum);
+}
+}
